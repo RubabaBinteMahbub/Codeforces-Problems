9f13b7d81e01ff1c6e50b0042e6e90cb3d7d9af8
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-10-20 21:12:29 +0600

    Update stonesOnThe_table.cpp

--- stonesOnThe_table.cpp
+++ stonesOnThe_table.cpp
@@ -3,13 +3,17 @@
 #include<algorithm>
 using namespace std;
 int main(){
-int n,count=0;
-cin>>n;
-string s;
-cin>>s;
-for(int i=0;i<s.size();i++){
-if(s[i]==s[i+1]) count++;
-}
-cout<<count<<endl;
-return 0;
+  int n,count=0;
+	cin>>n;
+	string s;
+	cin>>s;
+  
+	for(int i=0;i<s.size();i++){
+	if(s[i]==s[i+1]) count++;
+	}
+
+  // Edge case : if n==0, count==0.
+  
+	cout<<count<<endl;
+  return 0;
 }
