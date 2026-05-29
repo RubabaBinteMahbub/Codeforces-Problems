1c139afceba79504343dc2485068733cc576ef52
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-29 13:00:08 +0600

    Create template.cpp

---
+++ Template/template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define long long ll
+
+void LemonTea(){
+
+}
+
+int main()
+{
+// Fast I/O
+ios::sync_with_stdio(false);
+cin.tie(NULL);
+
+long long t;
+cin>>t;
+
+while(t--){
+  LemonTea();
+}
+
+return 0;
+}
